3960389d4f341ec0d270b8cfa6c53017bff3f673
WCram <wcram@rtc.edu>
2022-09-26 19:00:45 -0700

    Added ReadLine Info

--- Program.cs
+++ Program.cs
@@ -10,10 +10,26 @@ namespace Lecture_2_F22_In_Class
     {
         static void Main(string[] args)
         {
+
             // ReadLine
-            // Parse
+            // Console.ReadLine()
+            // It reads what types in and saves it
+            Console.Write("Please enter a number: ");
+
+            // This reads the users input
+            string userInput = Console.ReadLine();
+            // This only displays the string userInput
+            Console.WriteLine("userInput");
+            // This displays the variable
+            // The users number is {userInput}
+            Console.WriteLine("The users number is " + userInput);
+
             // Ints
+
+            // Parse
             // Math Operators
-        }
-    }
-}
+
+        } // Main
+
+    } // class
+} // namespace

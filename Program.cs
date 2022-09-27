de00d84dc7b0f45f4e71d6965c95fd262f5dd0c0
WCram <wcram@rtc.edu>
2022-09-26 19:41:38 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace Lecture_2_F22_In_Class
     {
         static void Main(string[] args)
         {
+            // ReadLineExample();
+            // IntAndMathExamples();
+            // Parse
+
+
+        } // Main
+
+        public static void ReadLineExample()
+        {
 
             // ReadLine
             // Console.ReadLine()
@@ -23,13 +32,79 @@ namespace Lecture_2_F22_In_Class
             // This displays the variable
             // The users number is {userInput}
             Console.WriteLine("The users number is " + userInput);
+        } // ReadLineExample()
 
-            // Ints
+        public static void IntAndMathExamples()
+        {
 
-            // Parse
+            // Integer - Intergral - Type - Number - Whole Numbers
+            int num1 = 6;
+            int num2 = 42;
+            int num3 = 3;
+
+            int sum = num1 + num2 + num3;
+
+            // .ToString() to covert numbers into strings
+            Console.WriteLine(num1.ToString());
+
+            // Number 1 is {num1}
+            // Number 2 is {num2}
+            // Number 3 is {num3}
+            // The sum is {num1}
+            //Console.WriteLine(
+            //    "Number 1 is " + num1 + "\n" +
+            //    "Number 2 is " + num2 + "\n" +
+            //    "Number 3 is " + num3 + "\n" +
+            //    "The sum of the 3 numbers is " + sum);
+
+            // String Literal
+            Console.WriteLine($"Number 1 is {num1}\n" +
+                $"Number 2 is {num2}\n" +
+                $"Number 3 is {num3}\n" +
+                $"The sum of the 3 numbers is {sum}");
+
             // Math Operators
+            // Plus +
+            sum = num1 + num2 + num3;
+            // Minus - 
+            int minus = num3 - num2 - num1;
+            // Multiply *
+            int multi = num2 * num1 * num3;
+            // Divide /
+            int divide = num3 / num2 / num1;
+            // Modulus - Mod - %
+            int mod = num3 % num2 % num1;
 
-        } // Main
+
+            Console.WriteLine(
+                $"Plus {sum}\n" +
+                $"Minus {minus}\n" +
+                $"Multiply {multi}\n" +
+                $"Divide {divide}\n" +
+                $"Modulus {mod}\n");
+
+            // Divide
+            int num4 = 5;
+            int num5 = 3;
+            int div2 = num4 / num5;
+            Console.WriteLine($"{num4} / {num5} = {div2}");
+
+            // Mod
+            int mod2 = num4 % num5;
+            Console.WriteLine($"{num4} % {num5} = {mod2}");
+
+            // How to find even or odd
+            int even = 544;
+            int odd = 58347;
+
+            bool isEven = (even % 2) == 0;
+
+            Console.WriteLine($"{even} is an even number - {even % 2 == 0}");
+            Console.WriteLine($"{even} is an even number - {even % 2 == 1}");
+            Console.WriteLine($"{odd} is an odd number - {odd % 2 == 0}");
+            Console.WriteLine($"{odd} is an odd number - {odd % 2 == 1}");
+
+        } // IntAndMathExamples
 
     } // class
 } // namespace
